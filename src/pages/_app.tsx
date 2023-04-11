0bfe4dc54567a0cdbe2a544837fbcb15709bad5d
minju1009 <minju1009@gmail.com>
2023-04-11 21:06:08 +0900

    chore: 샘플 페이지 추가

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app';
+// import { SessionProvider } from 'next-auth/react';
+import { useState } from 'react';
+import { RecoilRoot } from 'recoil';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import GlobalStyle from 'styles/GlobalStyle';
+import Layout from './components/Layout';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools initialIsOpen />
+        <GlobalStyle />
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
