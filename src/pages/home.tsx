b5a437b593f1b4bd26750066a77e71009c23821b
Nahyun Lee <lnhyen43@gmail.com>
2023-04-16 18:23:49 +0900

    CHORE: antd install & test card ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,22 @@
 import Layout from './components/Layout';
+import { Card, Space } from 'antd';
 
 export default function Home() {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <Space direction="vertical" size={16}>
+        <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Space>
+    </>
+  );
 }
